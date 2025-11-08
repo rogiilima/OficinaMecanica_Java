2cc53b10e94f0c1ac6f48a90e258dbab44042fd6
Matheus <joaosobramatheus@gmail.com>
2025-11-08 16:56:23 -0300

    <feat> Fazendo conexão com banco de dados MySQL

--- src/DB/ConexaoComBanco.java
+++ src/DB/ConexaoComBanco.java
@@ -1,6 +1,60 @@
 package DB;
 
+import java.sql.*;
 
 public class ConexaoComBanco {
+    private final String DRIVE = "com.mysql.jdbc.Driver";
+    private final String URL = "jdbc:mysql://localhost:3306/oficina";
+    private final String USER = "root";
+    private final String PASS = "";
+
+    // metodo de conexão com o banco de dados
+    public Connection getConnection(){
+        try {
+            Class.forName(DRIVE);
+
+            return DriverManager.getConnection(URL,USER,PASS);
+
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Erro na conexão do Banco De Dados: ",e);
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro na conexão do Banco De Dados: ",e);
+        }
+    }
+
+    public static void fechaConexao(Connection connection){
+        try{
+            if (connection != null){
+                connection.close();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Não fechou o banco de dados: ", e);
+        }
+    }
+
+
+    public static void fechaConexao(Connection connection, PreparedStatement ps){
+
+        fechaConexao(connection);
+        try{
+            if (ps != null){
+                ps.close();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Não fechou o banco de dados: ", e);
+        }
+    }
+    // uso de sobrecarga para tratar os diversos tipos de fechamentos
+    public static void fechaConexao(Connection connection, PreparedStatement ps, ResultSet rs){
+
+        fechaConexao(connection, ps);
+        try{
+            if (rs != null){
+                rs.close();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Não fechou o banco de dados: ", e);
+        }
+    }
 
 }
