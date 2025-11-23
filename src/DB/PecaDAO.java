cd59a12d96fa1a5620d8a528e5a1e0f8c8b13255
Matheus <joaosobramatheus@gmail.com>
2025-11-23 13:33:39 -0300

    <feat> Completa o CriarOrdemServicoController

--- src/DB/PecaDAO.java
+++ src/DB/PecaDAO.java
@@ -1,6 +1,8 @@
 package DB;
 
 import Model.Peca;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -38,7 +40,39 @@ public class PecaDAO {
         } finally {
             ConexaoComBanco.fechaConexao(conexao, stmt, rs);
         }
-
         return null;
     }
+
+    public static ObservableList<Peca> listarPecas() {
+        ObservableList<Peca> listaPecas = FXCollections.observableArrayList();
+        Connection conexao = ConexaoComBanco.getConnection();
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+
+        try {
+            stmt = conexao.prepareStatement("SELECT * FROM peca ORDER BY nome_peca");
+            rs = stmt.executeQuery();
+
+            DecimalFormat df = new DecimalFormat("#,##0.00");
+
+            while (rs.next()) {
+                Peca peca = new Peca(
+                        rs.getString("id_peca"),
+                        rs.getString("nome_peca"),
+                        df.format(rs.getDouble("preco_unitario")),
+                        rs.getString("quantidade_estoque")
+                );
+                listaPecas.add(peca);
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Erro ao listar peças: " + e.getMessage());
+        } finally {
+            ConexaoComBanco.fechaConexao(conexao, stmt, rs);
+        }
+
+        return listaPecas;
+
+    }
+
 }
